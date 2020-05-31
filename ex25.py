54b4bd9232b094715e2f49e3feafbbbf6dfc600d
wanghao18 <selfpoised@gmail.com>
2020-05-31 22:05:17 +0800

    add file

---
+++ ex25.py
@@ -0,0 +1,17 @@
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+
+if 2 > 1:
+    print("hello world")
+elif 2 < 1:
+    print("hello not")
+else:
+    print("no")
