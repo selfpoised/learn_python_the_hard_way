16546a99c281010b9563fa86d3d46a768650f35c
wanghao18 <selfpoised@gmail.com>
2020-05-31 11:33:54 +0800

    minor

---
+++ ex13.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print(f"script:{script} first:{first} second:{second} third:{third}")
---
+++ ex15.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+# script, filename = argv
+
+txt = open("ex15_example.txt")
+# print(txt.read())
+
+lines = txt.readlines()
+
+txt.close()
