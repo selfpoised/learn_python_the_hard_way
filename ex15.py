53a62216ec86e8aa4f537923867ec8dea94ee537
wanghao18 <selfpoised@gmail.com>
2020-05-31 11:44:20 +0800

    modify

--- ex15.py
+++ ex15.py
@@ -2,7 +2,8 @@ from sys import argv
 
 # script, filename = argv
 
-txt = open("ex15_example.txt")
+txt = open("ex15_example.txt", 'a+')
+txt.write("hello")
 # print(txt.read())
 
 lines = txt.readlines()
