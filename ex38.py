1eae091dcd0ea0e2954828b2c669b386d81d19c0
wanghao18 <selfpoised@gmail.com>
2020-06-03 16:49:26 +0800

    add ex38

---
+++ ex38.py
@@ -0,0 +1,36 @@
+states = {
+'Oregon': 'OR',
+'Florida': 'FL',
+'California': 'CA',
+'New York': 'NY',
+'Michigan': 'MI'
+}
+
+for k,v in states.items():
+    print(f"{k}:{v}")
+
+x = states.get('x')
+
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+print("Wait there are not 10 things in that list. Let's fix that.")
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+    next_one = more_stuff.pop()
+    print("Adding: ", next_one)
+    stuff.append(next_one)
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go:", stuff)
+
+print("Let's do some things with stuff.")
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(' '.join(stuff))
+print('#'.join(stuff[3:5]))
+
+print('#'.join(stuff[1:5]))
